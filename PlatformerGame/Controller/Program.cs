bc3191b55f97f36f7a770a2b2130ce32e07791d8
mattpow35 <mpow2231@740s-pl-121.local>
2017-05-18 09:56:06 -0600

    fixed things in content folder to allow game to run

--- PlatformerGame/Controller/Program.cs
+++ PlatformerGame/Controller/Program.cs
@@ -11,7 +11,7 @@ using UIKit;
 #endif
 #endregion
 
-namespace PlatformerGame
+namespace Platformer
 {
 #if __IOS__ || __TVOS__
     [Register("AppDelegate")]
@@ -20,11 +20,11 @@ namespace PlatformerGame
 	static class Program
 #endif
 	{
-		private static Game1 game;
+		private static PlatformerGame game;
 
 		internal static void RunGame()
 		{
-			game = new Game1();
+			game = new PlatformerGame();
 			game.Run();
 #if !__IOS__ && !__TVOS__
 			game.Dispose();
